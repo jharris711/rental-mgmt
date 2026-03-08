dd4d8d5f16abccc214d205c9d03d7da08d21fcab
Josh Harris <js.harris711@gmail.com>
2026-03-08 04:05:27 -0400

    Cleanups

--- ui/src/components/AppNavbar.vue
+++ ui/src/components/AppNavbar.vue
@@ -3,20 +3,25 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Rental Management</a>
+            <RouterLink
+                class="navbar-brand"
+                to="/"
+            >
+                Rental Management
+            </RouterLink>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a 
+                        <RouterLink
                         class="nav-link active"
                         aria-current="page"
-                        href="/"
+                        to="/"
                         >
-                        Home
-                        </a>
+                            Home
+                        </RouterLink>
                     </li>
                 </ul>
                 <div class="d-flex gap-2">
